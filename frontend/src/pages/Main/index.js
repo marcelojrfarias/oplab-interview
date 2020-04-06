e3519aa354415d89ce4209cee5753167496bb0c8
marcelojrfarias <marcelojrfarias@outlook.com>
2020-04-06 19:00:24 -0300

    Adds the app icon to the interface

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 import './styles.css'
 
+import appIcon from '../../assets/app_icon.png'
+
 export default function Main () {
   return (
     <div className="container">
 
-      <div className="side left">
-      </div>
+      <div className="side left"/>
       
       <div className="side right">
+        <div className="app-icon">
+          <img className="icon" src={appIcon} alt="Logo"/>
+        </div>
       </div>
       
     </div>
